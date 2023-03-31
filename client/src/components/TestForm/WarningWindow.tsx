b11b04654c65494278261d5b8e60b4463faa053a
skyhanull <anky0425@naver.com>
2023-03-31 17:02:24 +0900

    add: 새로고침해도 warning페이지가 나올시 useeffect로 변경해줌

--- client/src/components/TestForm/WarningWindow.tsx
+++ client/src/components/TestForm/WarningWindow.tsx
@@ -1,12 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import TypeForm from './TypeForm';
 import * as S from './WarningWindow.style';
+import { Link, useParams } from 'react-router-dom';
+import { isStart } from '../../atoms';
+import { useRecoilState } from 'recoil';
 
 const WarningWindow = () => {
   const [isWarning, setIsWarning] = useState<boolean>(false);
+  const [isStarting, setIsStarting] = useRecoilState(isStart);
+  const { name } = useParams();
 
+  useEffect(() => {
+    if (name === 'start') {
+      setIsStarting(false);
+    }
+  }, []);
   const WaringHandler = () => {
-    setIsWarning(!isWarning);
+    setIsWarning(true);
   };
   return (
     <S.WarningLargeContent>
@@ -17,7 +27,9 @@ const WarningWindow = () => {
           <S.WarnTitle>주의!</S.WarnTitle>
           <S.WarnContent>이 테스트는 정확한 테스트가 아니므로 주의해주십시오.</S.WarnContent>
           <S.WaringButtonContent>
-            <S.WarningButton onClick={WaringHandler}>Next</S.WarningButton>
+            <Link to="/test/1">
+              <S.WarningButton onClick={WaringHandler}>Next</S.WarningButton>
+            </Link>
           </S.WaringButtonContent>
         </div>
       )}
